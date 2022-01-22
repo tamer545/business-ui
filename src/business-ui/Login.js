65c49d43aa8986448a0aad22be65970162614b62
kaibria <kai@bria.ch>
2022-01-22 16:30:35 +0100

    feat: connect database and add login page

--- src/business-ui/Login.js
+++ src/business-ui/Login.js
@@ -1,1 +1,64 @@
-//TODO Implement Login Page
+import {TextField} from "@mui/material";
+import Button from "@mui/material/Button";
+import { useNavigate } from 'react-router-dom';
+import * as React from "react";
+import {useEffect, useState} from "react";
+import firebase from "firebase/compat";
+
+export default function Login() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [pwFromDatabase, setPwFromDatabase] = useState('')
+    const [errorMessage, setErrorMessage] = useState("")
+    const navigate = useNavigate();
+
+    function onLogin() {
+        findPassword()
+    }
+
+    useEffect(() => {
+        if (password !== '' || pwFromDatabase !== '') {
+            if (password === pwFromDatabase) {
+                console.log("login successful")
+                setErrorMessage("")
+                navigate("/business")
+            } else {
+                setErrorMessage("Incorrect password")
+
+            }
+        }
+    }, [pwFromDatabase])
+
+    function findPassword() {
+        // on() method
+        firebase.database().ref('users/' + username + '/security').on('value', (snap) => {
+            if (snap.val()) {
+                console.log("snap.val()", snap.val())
+                setPwFromDatabase(snap.val().password)
+            } else {
+                setErrorMessage("Incorrect Username")
+
+            }
+        });
+    }
+
+    return (
+        <div>
+            <TextField
+                sx={{width: '600px'}}
+                value={username}
+                placeholder={"Benutzer Name"}
+                onChange={e => setUsername(e.target.value)}
+            />
+            <TextField
+                sx={{width: '600px'}}
+                value={password}
+                type={"password"}
+                placeholder={"Password"}
+                onChange={e => setPassword(e.target.value)}
+            />
+                <Button variant={"contained"} onClick={() => onLogin()}>Login</Button>
+        </div>
+    );
+
+}
--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase/compat/app";
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAM1vEEvn0XmqGLDYrSGaW-xnv8ogOT-Ok",
+    authDomain: "biz-trips.firebaseapp.com",
+    databaseURL: "https://biz-trips-default-rtdb.firebaseio.com",
+    projectId: "biz-trips",
+    storageBucket: "biz-trips.appspot.com",
+    messagingSenderId: "676070572591",
+    appId: "1:676070572591:web:c3829d8af4edef03c0fcda",
+    measurementId: "G-D32CCJ4FF0"
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+        <App/>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
