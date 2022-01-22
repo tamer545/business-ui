aef1a49073f73d626bd4c44e38c31b492c573eaa
tamer545 <timo-angst@bluewin.ch>
2022-01-22 14:59:53 +0100

    feat: add registration and navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from './business-ui/SearchBar'
+import Login from "./business-ui/Login";
+import Registration from "./business-ui/Registration";
+import MeetingList from "./business-ui/MeetingList";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+
 function App() {
 
-  //TODO Implement Routing over different Sites
-  return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Router>
-  );
+    //TODO Implement Routing over different Sites
+    return (
+        <>
+            <SearchBar/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={'/login'} element={<Login/>}/>
+                    <Route path={'/registration'} element={<Registration/>}/>
+                    <Route path={'/business'} element={<MeetingList/>}/>
+                    <Route path={'/'} element={<Login/>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
+        ;
 }
 
 export default App;
